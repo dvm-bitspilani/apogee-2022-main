caed5dafb4bf39710898e4ab381d695027a7655a
rutva9l <rutva943@gmail.com>
2022-02-12 00:13:24 +0530

    buildings

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,18 @@ let hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function(){
   hamburger.classList.toggle('open');  
 })
+
+window.onload = function(){
+  let cities = document.querySelectorAll(".city");
+  
+  // for(let i = 2; i<6; i++){
+  //   cities[i].style.transition = "1.5s"
+  // }
+
+  cities[0].style.transform = "translate(-48%, 0)";
+  cities[1].style.transform = "translate(-48%, 0)";
+  cities[2].style.transform = "translate(-53%, 15%)";
+  cities[3].style.transform = "translate(-56%, 8%)";
+  cities[4].style.transform = "translate(-80%, 5%)";
+  cities[5].style.transform = "translate(-5%, -3%)";
+}
