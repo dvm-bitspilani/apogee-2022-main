0a7a7ae15c55b5112ba88c685d52df3c9fc12fad
Alvin Uday <udayalvin@gmail.com>
2022-02-25 16:44:22 +0530

    switch to react

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
+window.onload = function () {
+  let cities = document.querySelectorAll(".city");
+
+  console.log(cities);
+
+  for (let i = 0; i < 6; i++) {
+    cities[i].style.transition = "1.5s"
+    console.log(cities[i]);
+  }
+  cities[0].style.transform = "translate(-45%, 0)";
+  setTimeout(() => {
+    cities[1].style.transform = "translate(-45%, 0)";
+  }, 200);
+  setTimeout(() => {
+    cities[2].style.transform = "translate(-50%, 15%)";
+  }, 1000);
+  setTimeout(() => {
+    cities[3].style.transform = "translate(-53%, 8%)";
+  }, 1800);
+  setTimeout(() => {
+    cities[4].style.transform = "translate(-75%, 5%)";
+  }, 2600);
+  setTimeout(() => {
+    cities[5].style.transform = "translate(-2%, -3%)";;
+  }, 3200);
+
+
+  // cities[2].style.transform = "translate(-50%, 15%)";
+  // cities[3].style.transform = "translate(-53%, 8%)";
+  // cities[4].style.transform = "translate(-75%, 5%)";
+  // cities[5].style.transform = "translate(-2%, -3%)";
+}
+
+console.log("HAHAHHAHAHHAH");
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
