80ec2212d4e8589af0ad9c36196325eb6d498679
rutva9l <rutva943@gmail.com>
2022-03-05 00:33:56 +0530

    nav link

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -3,7 +3,30 @@ import { Link } from 'react-router-dom';
 import Dummy from '../assets/dummy.svg';
 import ViewBtn from '../assets/Button.svg'
 import Arrow from '../assets/Arrox.svg'
+import Modal from '@mui/material/Modal';
+import { styled, Box } from '@mui/system';
+
+
 import "../events.css"
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '60vw',
+    height: '60vh',
+    bgcolor: 'rgba(7, 9, 73, 0.09)',
+    border: '1px solid rgba(209, 213, 219, 0.3);',
+    backdropFilter: 'blur(20px)',
+    borderRadius: '20px',
+    padding: '50px 100px',
+
+
+    // boxShadow: 24,
+    // p: 5,
+    // filter:'blur(3px) saturate(180%)'
+};
 
 function Events() {
     const [events, setEvents] = React.useState([])
@@ -24,6 +47,10 @@ function Events() {
                 setEvents(result.data[0].events);
             });
     }, [])
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
         <div>
             <div className="bg"></div>
@@ -39,66 +66,26 @@ function Events() {
                             <div className="card-text">
                                 <h3>{el.name}</h3>
                                 <p>{el.details}</p>
-                                <div className="view-btn">View Details</div>
+                                <div className="view-btn" onClick={handleOpen}>View Details</div>
+                                <div className="ModalBox">
+
+                                    <Modal
+                                        open={open}
+                                        onClose={handleClose}
+                                        aria-labelledby="modal-modal-title"
+                                        aria-describedby="modal-modal-description"
+                                    >
+                                        <Box sx={style}>
+                                            <div className="modalHeading">
+                                                {el.name}
+                                            </div>
+
+                                        </Box>
+                                    </Modal>
+                                </div>
                             </div>
                         </div>
                     )}
-                    <div className="card">
-                        <div className="card-img"><img src={Dummy} /></div>
-                        <div className="card-text">
-                            <h3>Heading</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <div className="view-btn">View Details</div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card-img"><img src={Dummy} /></div>
-                        <div className="card-text">
-                            <h3>Heading</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <div className="view-btn">View Details</div>
-                        </div>
-                    </div>
-                    {/* <div className="card">
-                        <div className="card-img"><img src={Dummy} /></div>
-                        <div className="card-text">
-                            <h3>Heading</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <div className="view-btn">View Details</div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card-img"><img src={Dummy} /></div>
-                        <div className="card-text">
-                            <h3>Heading</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <div className="view-btn">View Details</div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card-img"><img src={Dummy} /></div>
-                        <div className="card-text">
-                            <h3>Heading</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <div className="view-btn">View Details</div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card-img"><img src={Dummy} /></div>
-                        <div className="card-text">
-                            <h3>Heading</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <div className="view-btn">View Details</div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card-img"><img src={Dummy} /></div>
-                        <div className="card-text">
-                            <h3>Heading</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <div className="view-btn">View Details</div>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </div>
