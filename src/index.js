c91d8df7c2504076b5852db224c0041b21e30a1f
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2022-03-17 10:28:09 +0530

    fix: handlecities

--- src/index.js
+++ src/index.js
@@ -9,39 +9,35 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./fonts/BARTKEY.ttf";
 
-// function handleCities() {
-//   let cities = document.querySelectorAll(".city");
+function handleCities() {
+  let cities = document.querySelectorAll(".city");
 
-//   for (let i = 0; i < 6; i++) {
-//     cities[i].style.transition = "1.5s";
-//   }
-//   cities[0].style.transform = "translate(-45%, 0)";
-//   setTimeout(() => {
-//     cities[1].style.transform = "translate(-46%, 0)";
-//   }, 200);
-//   setTimeout(() => {
-//     cities[2].style.transform = "translate(-50%, 15%)";
-//   }, 1000);
-//   setTimeout(() => {
-//     cities[3].style.transform = "translate(-54%, 8%)";
-//   }, 1800);
-//   setTimeout(() => {
-//     cities[4].style.transform = "translate(-75%, 5%)";
-//   }, 2600);
-//   setTimeout(() => {
-//     cities[5].style.transform = "translate(-7%, -3%)";
-//   }, 3200);
+  for (let i = 0; i < 6; i++) {
+    cities[i].style.transition = "1.5s";
+  }
+  cities[0].style.transform = "translate(-45%, 0)";
+  setTimeout(() => {
+    cities[1].style.transform = "translate(-46%, 0)";
+  }, 200);
+  setTimeout(() => {
+    cities[2].style.transform = "translate(-50%, 15%)";
+  }, 1000);
+  setTimeout(() => {
+    cities[3].style.transform = "translate(-54%, 8%)";
+  }, 1800);
+  setTimeout(() => {
+    cities[4].style.transform = "translate(-75%, 5%)";
+  }, 2600);
+  setTimeout(() => {
+    cities[5].style.transform = "translate(-7%, -3%)";
+  }, 3200);
+}
 
-//   // cities[2].style.transform = "translate(-50%, 15%)";
-//   // cities[3].style.transform = "translate(-53%, 8%)";
-//   // cities[4].style.transform = "translate(-75%, 5%)";
-//   // cities[5].style.transform = "translate(-2%, -3%)";
-// }
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<App handleCities={handleCities} />} />
       <Route path="/events" element={<Events />} />
       <Route path="/Test" element={<Test />} />
 
@@ -51,9 +47,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// window.onload = handleCities();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
