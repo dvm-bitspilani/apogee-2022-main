ea9e178b20aa8ee502f9c9a72b02ec6696c26a7a
Alvin Uday <udayalvin@gmail.com>
2022-03-18 19:42:27 +0530

    changes

--- src/Pages/Test.jsx
+++ src/Pages/Test.jsx
@@ -1,12 +1,20 @@
+// imports
 import MainHero from "../Components/MainHero/MainHero.jsx";
 import NavBar from "../Components/Navbar/Navbar.jsx"
 
+// CSS
+import "../stylesheets/Test.css"
 
 function Test() {
     return (
         <div className="testWrapper">
-            <NavBar/>
-            <MainHero/>
+            <section className="section" id="landingSection">
+                <NavBar />
+                <MainHero />
+            </section>
+            <section className="section" id="kernelSection">
+                KERNEL SECTION
+            </section>
         </div>
     )
 }
