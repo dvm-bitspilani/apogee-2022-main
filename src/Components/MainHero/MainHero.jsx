5477badad7033e7f609e6481aa6522193ebe8ec1
Alvin Uday <udayalvin@gmail.com>
2022-03-22 23:32:00 +0530

    possible address bar fix

--- src/Components/MainHero/MainHero.jsx
+++ src/Components/MainHero/MainHero.jsx
@@ -23,18 +23,28 @@ function MainHero() {
 
     useEffect(() => {
         console.log("hello");
+        window.onwheel = e => {
+            if (e.deltaY >= 0) {
+                // Scrolling Down with mouse
+                console.log('Scroll Down');
+                // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+                let vh = window.innerHeight * 0.01;
+                // Then we set the value in the --vh custom property to the root of the document
+                document.documentElement.style.setProperty('--vh', `${vh}px`);
+                // We listen to the resize event
+                window.addEventListener('resize', () => {
+                    // We execute the same script as before
+                    let vh = window.innerHeight * 0.01;
+                    document.documentElement.style.setProperty('--vh', `${vh}px`);
+                });
+            } else {
+                // Scrolling Up with mouse
+                console.log('Scroll Up');
+            }
+        }
         const screenWidth = window.innerWidth;
         const screenHeight = window.innerHeight;
-        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-        let vh = window.innerHeight * 0.01;
-        // Then we set the value in the --vh custom property to the root of the document
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-        // We listen to the resize event
-        window.addEventListener('resize', () => {
-            // We execute the same script as before
-            let vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
-        });
+
 
         Layer1.current.style.transform = "bottom: 0";
         console.log(Layer1.current, screenWidth, screenHeight);
