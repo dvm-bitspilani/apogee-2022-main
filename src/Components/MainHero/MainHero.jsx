a2c2865e6bb3915980fe4c4b8534f213a3b37588
Alvin Uday <udayalvin@gmail.com>
2022-03-22 23:39:43 +0530

    test

--- src/Components/MainHero/MainHero.jsx
+++ src/Components/MainHero/MainHero.jsx
@@ -23,26 +23,26 @@ function MainHero() {
 
     useEffect(() => {
         console.log("hello");
-        window.onwheel = e => {
-            if (e.deltaY >= 0) {
-                // Scrolling Down with mouse
-                console.log('Scroll Down');
+        // window.onwheel = e => {
+        //     if (e.deltaY >= 0) {
+        //         // Scrolling Down with mouse
+        //         console.log('Scroll Down');
                 
-            } else {
-                // Scrolling Up with mouse
-                console.log('Scroll Up');
-            }
-            // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+        //     } else {
+        //         // Scrolling Up with mouse
+        //         console.log('Scroll Up');
+        //     }
+        // }
+        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+        let vh = window.innerHeight * 0.01;
+        // Then we set the value in the --vh custom property to the root of the document
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        // We listen to the resize event
+        window.addEventListener('resize', () => {
+            // We execute the same script as before
             let vh = window.innerHeight * 0.01;
-            // Then we set the value in the --vh custom property to the root of the document
             document.documentElement.style.setProperty('--vh', `${vh}px`);
-            // We listen to the resize event
-            window.addEventListener('resize', () => {
-                // We execute the same script as before
-                let vh = window.innerHeight * 0.01;
-                document.documentElement.style.setProperty('--vh', `${vh}px`);
-            });
-        }
+        });
         const screenWidth = window.innerWidth;
         const screenHeight = window.innerHeight;
 
