f839da73a0e94463241a07757b344b2b8a69ca1a
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2022-03-25 01:02:26 +0530

    to pull

---
+++ src/Components/ContactProfileCard/ContactProfileCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ContactProfileCard.css';
+
+export default function ContactProfileCard() {
+  return (
+    <div>
+        <h1>CONTACT US</h1>
+    </div>
+  )
+}
