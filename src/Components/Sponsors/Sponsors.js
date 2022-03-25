a599a180b5d2af60aca8ac6524fee95589721b53
Priyanshu <priyanshushukla9801@gmail.com>
2022-03-25 07:12:46 +0530

    Sponsors Back Button

--- src/Components/Sponsors/Sponsors.js
+++ src/Components/Sponsors/Sponsors.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 import "./styles.css";
 import Arrow from "../../assets/Arrox.svg";
 import LoaderPage from "../../Pages/loaderPage";
