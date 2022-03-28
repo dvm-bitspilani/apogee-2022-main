2990c6f182d0afa353822e73ff5d415291ce0da4
sarthakeash <f20200060@pilani.bits-pilani.ac.in>
2022-03-28 19:04:08 +0530

    carousel done

--- src/Components/SpeakerSection/SpeakerCard.jsx
+++ src/Components/SpeakerSection/SpeakerCard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Photo from "./speakerPhotos/Speaker_image.png"
 import './SpeakerCard.css'
-function SpeakerCard() {
+function SpeakerCard(props) {
   
     return (
         <div className="speakerCardMain">
             <div className="speakerInfo">
                 <div className="speakerPhoto">
-                    <img src={Photo} alt="" />
+                    <img src={require(`./../../assets/speakerPhotos/${props.profileImage}.png`)} alt="" />
                 </div>
                 <div className="speakerPersonal">
-                    <h2> Dr. Soumya Swaminathan</h2>
-                    <p>Chief Scientist, World Health Organisation</p>
+                    <h2> {props.name}</h2>
+                    <p>{props.bio}</p>
                 </div>
             </div>
             <div className="speakerDetails">
- <p>lorem ipsum, dolor sit amet consectetur adipisicing eliaerat fugdipisci reprehenderit reiciendis architecto nobis maxime velit exercitationem repudiandae ipsam! Sapiente ab quidem ex architecto. Itaque exercitationem voluptas, praesentium adipisci expedita ullam deleniti, soluta aperiam accusamus repellendus magni error neque esse est laborum?</p> 
+                <p>{props.details}</p> 
                 
             </div>
     </div>
--- src/Components/SpeakerSection/SpeakerSection.jsx
+++ src/Components/SpeakerSection/SpeakerSection.jsx
@@ -1,6 +1,48 @@
 // styles
 import "./SpeakerSection.css"
 import SpeakerCard from "./SpeakerCard.jsx"
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+
+
+const speakers = [{
+    id:1,
+    name: "Dr. Soumya Swaminathan",
+    bio: "Chief Scientist, World Health Organisation",
+    details: "Lorem ipsum dolor sit amet, cotetur adipiscing elit. Consequat, ut egestas vel venes hac euismod. In magna lorem imperdiet amet commodo. Suspendisse purus vitae mi sit sollicitudin ultrices. Eros t Suspendisse purus vitae mi sit sollicitudin ultrices Suspendisse purus viltrices",
+    profileImage: "Speaker_image"
+
+},
+    {
+    id:2,
+    name: "Dr. Soumya Swaminathan",
+    bio: "Chief Scientist, World Health Organisation",
+    details: "Lorem ipsum dolor sit amet, cotetur adipiscing elit. Consequat, ut egestas vel venes hac euismod. In magna lorem imperdiet amet commodo. Suspendisse purus vitae mi sit sollicitudin ultrices. Eros t Suspendisse purus vitae mi sit sollicitudin ultrices Suspendisse purus viltrices",
+    profileImage: "Speaker_image"
+
+    },
+    {
+        id:3,
+    name: "Dr. Soumya Swaminathan",
+    bio: "Chief Scientist, World Health Organisation",
+    details: "Lorem ipsum dolor sit amet, cotetur adipiscing elit. Consequat, ut egestas vel venes hac euismod. In magna lorem imperdiet amet commodo. Suspendisse purus vitae mi sit sollicitudin ultrices. Eros t Suspendisse purus vitae mi sit sollicitudin ultrices Suspendisse purus viltrices",
+    profileImage: "Speaker_image"
+
+    }]
+
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+};
+const styles = {
+          
+
+    backgroundColor:"transparent"
+      }
 
 
 function SpeakerSection() {
@@ -11,7 +53,11 @@ function SpeakerSection() {
             </div>
             <div className="subHeading">Think Again Live</div>
             <div className="speakerCarousel">
-                <SpeakerCard />   
+           <AwesomeSlider  cssModule={styles} bullets={false}>
+                {speakers.map(e => {
+                    return <div key={e.id}><SpeakerCard name={e.name} bio={e.bio} profileImage={e.profileImage} details={e.details}/></div> 
+              })}
+             </AwesomeSlider>
           </div>
         </div>
     )
