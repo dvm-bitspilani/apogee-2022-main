80f2685a694b7e7c65f3dd0a38cde4565067939d
sarthakeash <f20200060@pilani.bits-pilani.ac.in>
2022-03-29 13:24:37 +0530

    minor comment out

--- src/Components/SpeakerSection/SpeakerSection.jsx
+++ src/Components/SpeakerSection/SpeakerSection.jsx
@@ -62,7 +62,7 @@ function SpeakerSection() {
                         })}
 
                     </Slider>
-                    <DotGroup onClick={handleClick} />
+                    <DotGroup />
 
                 </CarouselProvider>
 
