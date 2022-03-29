23d8572b666608f3d0327cf0ac5fe049cf48769f
rutva9l <rutva943@gmail.com>
2022-03-29 12:56:45 +0530

    paginaton dynamic

--- src/Components/SpeakerSection/SpeakerSection.jsx
+++ src/Components/SpeakerSection/SpeakerSection.jsx
@@ -7,41 +7,41 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 
 
 const speakers = [{
-    id:1,
+    id: 1,
     name: "Dr. Soumya Swaminathan",
     bio: "Chief Scientist, World Health Organisation",
     details: "Chief Scientist at world health organization, She was the formal Deputy Director-General at WHO. She is now the Coordinator of the UNICEF/UNDP/WORLD BANK / WHO Special Programme and known for her Research on Tuberculosis and HIV.",
     profileImage: "soumya"
 
 },
-    {
-    id:2,
+{
+    id: 2,
     name: "Zainab Nagin Cox",
     bio: "US Spacecraft and Operations Engineer",
     details: "She is a US Spacecraft and Operations Engineer. She is also the tactical Mission Lead on Mars Missions Curiosity & Perseverance Rover. Asteroid 14061 has been-+ named 14061 Nagincox in her honour as its discoverer",
     profileImage: "zainab"
 
-    },
-    {
-        id:3,
+},
+{
+    id: 3,
     name: "Jeffrey Archer",
     bio: "Bestselling author",
     details: "He is one of the  Bestselling authors and wrote books like 'Kane and Abel', 'Not a Penny More, Not a penny less '. He was the former Deputy Chairman of the Conservative Party and Former Member of Parliament, UK.",
     profileImage: "jeffrey"
 
-    }, {
-        id:4,
+}, {
+    id: 4,
     name: "Dr. Henry Throop",
     bio: "American Astronomer-Consultant to NASA",
     details: "He is an American Astronomer and a Consultant to NASA. He was a Member of the first Pluto Mission Team, New Horizons. He is the Discoverer of Pluto's smallest moon, Styx, in 2012.Asteroid 193736 named Henrythroop in his honour.",
     profileImage: "henry"
 
-    }]
+}]
 
 
 
 function SpeakerSection() {
-        const carouselHeight=(window.screen.width<768)?140:40
+    const carouselHeight = (window.screen.width < 768) ? 140 : 40
 
     return (
         <div className="SpeakerWrapper">
@@ -50,23 +50,23 @@ function SpeakerSection() {
             </div>
             <div className="subHeading">Think Again Live</div>
             <div className="speakerCarousel">
-            <CarouselProvider
-        naturalSlideWidth={100}
-        naturalSlideHeight={carouselHeight}
-        totalSlides={4}
-      >
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={carouselHeight}
+                    totalSlides={4}
+                >
                     <Slider>
-         
-                        {speakers.map(e => {
-                    return <Slide index={e.id-1}><SpeakerCard name={e.name} bio={e.bio} profileImage={e.profileImage} details={e.details}/></Slide>
-              })}
-      
-                    </Slider>
-                    <DotGroup onClick={handleClick}/>
 
-      </CarouselProvider>
-         
-          </div>
+                        {speakers.map(e => {
+                            return <Slide index={e.id - 1}><SpeakerCard name={e.name} bio={e.bio} profileImage={e.profileImage} details={e.details} /></Slide>
+                        })}
+
+                    </Slider>
+                    <DotGroup onClick={handleClick} />
+
+                </CarouselProvider>
+
+            </div>
         </div>
     )
 }
