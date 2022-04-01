72f53ceda93a3e45fa77d1efbb9d459e3fdaad34
sarthakeash <f20200060@pilani.bits-pilani.ac.in>
2022-04-01 23:31:47 +0530

    minor

--- src/Components/RegModalButton/RegModalButton.js
+++ src/Components/RegModalButton/RegModalButton.js
@@ -422,8 +422,8 @@ export default function RegModalButton(props) {
                             value={name}
                             style={getStyles(name, eventName, theme)}
                           >
-                            {name}
-                            {/* {name == ("Armageddon" || "ARMAGEDDON") ? <Link to="/armageddon">{name}</Link> : name} */}
+                            {/* {name} */}
+                            {name == ("Armageddon" || "ARMAGEDDON") ? <Link to="/armageddon">{name}</Link> : name}
                           </MenuItem>
                         ))}
 
